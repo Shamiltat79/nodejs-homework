f2370f6696c6c4496c6bbdc8f0e36a43d9d36e87
Ruslan <shamiltat79@gmail.com>
2023-06-17 23:02:11 +0300

    owner

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -7,8 +7,10 @@ const {HttpError, ctrlWrapper} = require("../helpers");
 
 
 const getContacts = async (req, res) => {
-
-      const result  = await Contact.find({}, "-createdAt -updatedAt");
+  const {_id: owner} = req.user;
+  const {page = 1, limit = 10} = req.query;
+  const skip = (page - 1) * limit;
+  const result  = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "email name");
     res.json(result);
     
 }
@@ -28,8 +30,8 @@ const getContacts = async (req, res) => {
 
 const addContact = async (req, res) => {
     
-      
-      const result = await Contact.create(req.body);
+      const {_id: owner} = req.params;
+      const result = await Contact.create({...req.body, owner});
       res.status(201).json(result);
     
     
