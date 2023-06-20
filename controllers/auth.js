2f65f5353b77c81f492600858ac5d3e28698d4f3
Ruslan <shamiltat79@gmail.com>
2023-06-20 17:26:47 +0300

    jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
+const Jimp = require("jimp");
 
 const fs = require('fs/promises');
 const path = require('path');
@@ -98,11 +99,22 @@ const logout = async (req, res) => {
 const updateAvatar = async (req, res) => {
     const {_id} = req.user;
     const {path: tempUpload, originalname} = req.file;
+
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURL = path.join("avatars", filename);
-    await User.findByIdAndUpdate(_id, {avatarURL});
+    
+    const avatarURL = path.join("public", "avatars", filename);
+    const resizedAvatar =  Jimp.read(resultUpload)
+    .then(img => {return img.resize(250, 250).write(avatarURL);
+    })
+    .catch(error => {
+        throw new HttpError(404, `${error.message}`);
+    });
+    
+    
+   
+    await User.findByIdAndUpdate(_id, resizedAvatar);
 
     res.json({
         avatarURL,
